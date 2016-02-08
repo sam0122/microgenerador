13d4e9d1a4ecbed7843efe407b0cfc2b49acf223
sam0122 <sf.torres405@gmail.com>
2016-02-08 13:35:57 -0500

    Se agregó a Generador.m el area, el centroide y un radio medio equivalente

--- Generador.m
+++ Generador.m
@@ -15,10 +15,15 @@ function [Ag] = Generador(nAgreg,tamMed, tamDesv,incInf,incSup)
     %Area de cada agregado va en la tercera columna
     %Coordenadas del centroide del polígono, con respecto al centro del
     %cuadrado, van en la cuarta columna.
-    Ag = { zeros(nAgreg,4)};
+    Ag = { zeros(nAgreg,5)};
     Ag{:, 1} = normrnd(tamMed,tamDesv,[nAgreg,1]);
     Ag{:, 2} = unidrnd(incSup-incInf,nAgreg,1)+incInf;
     Ag{:, 3} = Ag{:,1}.^2+0.5.*(Ag{:,1}.*Ag{:,1}.*0.5.*tand(45));
     Ag{:,4} = zeros(nAgreg,2);
-    Ag{:,4}(:,2) = Ag{:, 1}.*0.5+(Ag{:, 1}.*0.5.*tand(45))./3;
+    Ag{:,4}(:,2) = ((Ag{:, 1}.*0.5+(Ag{:, 1}.*0.5.*tand(45))./3).*0.5.*Ag{:, 1}.*(0.5.*Ag{:, 1}.*tand(45)))./ Ag{:, 3};
+    %Ag{:,5}= sqrt(( Ag{:, 1}.*0.5+Ag{:,4}(:,2)).^2+(Ag{:,1}.*0.5).^2);
+    %Expresión que representa la distancia más grande que existe entre el
+    %centroide del polígono y el vértice más alejado. Almacenado en la
+    %quintaa columna
+    Ag{:,5} = Ag{:,1}.*0.5-Ag{:,4}(:,2)+Ag{:,1}.*0.5.*tand(45);
 end
